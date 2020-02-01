36c492b6f804498fbe426566d4d3302625ea44ef
Junqing <1551486328@qq.com>
2020-02-01 21:06:00 +0800

    formate

--- src/utils/Step.js
+++ src/utils/Step.js
@@ -1,33 +1,30 @@
 export default class Step {
-
-  constructor () {
+  constructor() {
     this.actived = false;
     this.queue = [];
   }
 
-  push (method, ...args) {
+  push(method, ...args) {
     let key;
     if (typeof method === 'string') {
-      key = args[0], method = args[1];
+      (key = args[0]), (method = args[1]);
     }
     if (typeof args[args.length - 1] !== 'function') {
-      args.push(function () {});
+      args.push(function() {});
     }
 
-    var callback = args[args.length - 1];
-
     this.queue.push({
-      'key': key || (this.queue.length - 1),
-      'method': method,
-      'args': args
+      key: key || this.queue.length - 1,
+      method: method,
+      args: args,
     });
 
     this.next();
     return this;
   }
 
-  next (key) {
-    if (!this.queue.length) return this.actived = false;
+  next(key) {
+    if (!this.queue.length) return (this.actived = false);
     if (this.actived) return;
     this.actived = true;
 
@@ -37,8 +34,8 @@ export default class Step {
     } else {
       item = this.queue.find(v => v.key === key);
     }
-    const {method, args} = item;
-    let callback = args[args.length - 1];
+    const { method, args } = item;
+    const callback = args[args.length - 1];
 
     args[args.length - 1] = (...rest) => {
       callback(...rest);
@@ -47,13 +44,12 @@ export default class Step {
     method(...args);
   }
 
-  _next () {
+  _next() {
     this.actived = false;
     this.next();
   }
 
-  goto (key) {
+  goto(key) {
     return this.next(key);
   }
-
 }
