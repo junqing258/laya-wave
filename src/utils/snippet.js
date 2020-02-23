2558adc428e6b7a8db34e349352198a75c7326bc
Junqing <1551486328@qq.com>
2020-02-23 18:50:27 +0800

    utils

--- src/utils/snippet.js
+++ src/utils/snippet.js
@@ -49,7 +49,31 @@ exports.throttle = (fn, wait) => {
   };
 };
 
+
 exports.difference = (a, b) => {
   const s = new Set(b);
   return a.filter(x => !s.has(x));
 };
+
+exports.deepClone = (data) => {
+  var t = type(data),
+    o, i, ni;
+  if (t === 'array') {
+    o = [];
+  } else if (t === 'object') {
+    o = {};
+  } else {
+    return data;
+  }
+  if (t === 'array') {
+    for (i = 0, ni = data.length; i < ni; i++) {
+      o.push(deepClone(data[i]));
+    }
+    return o;
+  } else if (t === 'object') {
+    for (i in data) {
+      o[i] = deepClone(data[i]);
+    }
+    return o;
+  }
+}
